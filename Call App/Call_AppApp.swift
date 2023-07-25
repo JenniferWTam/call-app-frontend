4f4097091c31d228811c12475c6660d4f206a78b
Jennifer Tam <jenniferwtam@gmail.com>
2023-07-24 22:12:59 -0400

    Initial Commit

---
+++ Call App/Call_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Call_AppApp.swift
+//  Call App
+//
+//  Created by Jennifer Tam on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Call_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Call AppUITests/Call_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Call_AppUITestsLaunchTests.swift
+//  Call AppUITests
+//
+//  Created by Jennifer Tam on 7/24/23.
+//
+
+import XCTest
+
+final class Call_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
