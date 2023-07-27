1cc08d3aa19eda8ddfaedf512fb3fe6d336ff1fb
Jennifer Tam <jenniferwtam@gmail.com>
2023-07-27 12:22:59 -0400

    Made changes to styling, Login saves phone number and name locally

--- Call App/ContentView.swift
+++ Call App/ContentView.swift
@@ -7,15 +7,167 @@
 
 import SwiftUI
 
+enum AuthenticationMethod {
+    case email
+    case phone
+    case gmail
+}
+
+enum LoginState: Hashable {
+    case loggedOut
+    case loggedIn(AuthenticationMethod, name: String, phoneNumber: String)
+}
+
 struct ContentView: View {
+    @State private var loginState: LoginState = .loggedOut
+    @AppStorage("name") private var name: String = ""
+    @AppStorage("phoneNumber") private var phoneNumber: String = ""
+    @State private var partySize = 1
+    @State private var reservationTime = Date()
+    @State private var restaurantName = ""
+    @State private var email: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                switch loginState {
+                case .loggedOut:
+                    LoginForm(loginState: $loginState, email: $email, name: $name)
+                case .loggedIn(_, let userName, let userPhoneNumber):
+                    ReservationForm(userName: userName, userPhoneNumber: userPhoneNumber, partySize: $partySize, name: $name, restaurantName: $restaurantName, reservationTime: $reservationTime)
+                }
+            }
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [Color(red: 204/255, green: 239/255, blue: 252/255), Color(red: 178/255, green: 218/255, blue: 251/255)]), startPoint: .top, endPoint: .bottom))
+            .navigationBarTitle("Reservation App")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(trailing: logoutButton)
+            .foregroundColor(.white)
+        }
+        .navigationViewStyle(StackNavigationViewStyle()) // (Optional) Use this line to apply the style for iOS 14 and below
+    }
+
+    private func logout() {
+        loginState = .loggedOut
+        name = ""
+        phoneNumber = ""
+        partySize = 1
+        reservationTime = Date()
+    }
+
+    private var logoutButton: some View {
+        Button(action: {
+            logout()
+        }) {
+            Text("Logout")
+        }
+    }
+}
+
+struct LoginForm: View {
+    @Binding var loginState: LoginState
+    @Binding var email: String
+    @Binding var name: String
+    @State private var phoneNumber: String = ""
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Sign in to make your reservation")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+
+            TextField("Username or Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .foregroundColor(.black)
+
+            TextField("Phone Number", text: $phoneNumber)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numberPad)
+                .foregroundColor(.black)
+
+            TextField("Name", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .foregroundColor(.black)
+
+            Button(action: {
+                // Successful login
+                self.loginState = .loggedIn(.email, name: self.name, phoneNumber: self.phoneNumber)
+            }) {
+                Text("Log In")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+
+            HStack(spacing: 5) {
+                Rectangle()
+//                    .fill(Color.gray)
+                    .frame(height: 1)
+
+                Text("OR")
+
+                Rectangle()
+//                    .fill(Color.gray)
+                    .frame(height: 1)
+            }
+            .padding(.horizontal)
+
+            Spacer()
+
+            HStack {
+                Text("Don't have an account?")
+                Button(action: {
+                    // Perform signup action here
+                }) {
+                    Text("Sign up.")
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 20)
         }
         .padding()
+    }
+}
+
+struct ReservationForm: View {
+    var userName: String
+    var userPhoneNumber: String
+    @Binding var partySize: Int
+    @Binding var name: String
+    @Binding var restaurantName: String
+    @Binding var reservationTime: Date
+
+    var body: some View {
+        Form {
+            Section(header: Text("Reservation Details")) {
+                Text("Name: \(userName)")
+                    .foregroundColor(.black) // Set font color to black
+                Text("Phone Number: \(userPhoneNumber)")
+                    .foregroundColor(.black) // Set font color to black
+                TextField("Restaurant", text: $restaurantName)
+                    .foregroundColor(.black)
+                Stepper("Party Size: \(partySize)", value: $partySize, in: 1...10)
+                    .foregroundColor(.black)
+                DatePicker("Date and Time", selection: $reservationTime, displayedComponents: [.date, .hourAndMinute])
+                    .foregroundColor(.black)
+            }
+        }
+        .accentColor(.black)
+        .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.blue.opacity(0.5)]), startPoint: .top, endPoint: .bottom))
+    }
+}
+
+extension LoginState {
+    func getAuthenticationMethod() -> AuthenticationMethod? {
+        switch self {
+        case .loggedIn(let authMethod, _, _):
+            return authMethod
+        default:
+            return nil
+        }
     }
 }
 
@@ -24,3 +176,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
